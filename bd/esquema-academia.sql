1218c592a9fc5591e2ffe72394209488d92d109f
rafaella-sa <34720730+rafaella-sa@users.noreply.github.com>
2019-03-01 15:18:20 -0300

    Add files via upload

---
+++ bd/esquema-academia.sql
@@ -0,0 +1,221 @@
+﻿
+INSERT INTO Aluno VALUES 
+  (1,'James','Borg','10-NOV-27', 'Stone',450,'Campinas', 'Houston',null);
+INSERT INTO Aluno VALUES 
+  (2,'Franklin','Wong','08-DEC-45','Voss',638 ,'Goianira','Houston',null);
+INSERT INTO Aluno VALUES 
+  (3,'Jennifer','Wallace','20-JUN-31','Berry',291,'Cidade Jardim' ,'Bellaire',null);
+INSERT INTO Aluno VALUES 
+  (4,'Jared','James','10-OCT-1966','Peachtree',123,'Centro',' Atlanta',null);
+INSERT INTO Aluno VALUES 
+  (5,'Alex','Freed','09-OCT-1950','Pillsbury',433,'Samambaia',' Milwaukee',null);
+INSERT INTO Aluno VALUES 
+  (6,'John','James','30-JUN-1975','Bloomington',7676,'Copacabana',' Sacramento',null);
+INSERT INTO Aluno VALUES 
+  (7,'Nandita','Ball','16-APR-1969','Howard',222,'Campinas',' Sacramento',null);
+INSERT INTO Aluno VALUES 
+  (8,'Bob','Bender','17-APR-1968','Garfield',8794,'Centro' ,'Chicago',null);
+INSERT INTO Aluno VALUES 
+  (9,'Jill','Jarvis','14-JAN-1966','Lincoln',6234,'Cidade Jardim',' Chicago',null);
+INSERT INTO Aluno VALUES 
+  (10,'Kate','King','16-APR-1966','Boone', 1976, 'Copacabana','Chicago',null);
+INSERT INTO Aluno VALUES 
+  (11,'Lyle','Leslie','09-JUN-1963','Hancock ',417,'Ave', 'Chicago',null);
+INSERT INTO Aluno VALUES 
+  (12,'Billie','King','01-JAN-1960','Washington',556,'Campinas',' Chicago',null);
+INSERT INTO Aluno VALUES 
+  (13,'Jon','Kramer','22-AUG-1964','Windy Creek',1988,'Samambaia',' Seattle',null);
+INSERT INTO Aluno VALUES 
+  (14,'Ray','King','16-AUG-1949','Delk Road',213,'Campinas','Seattle',null);
+INSERT INTO Aluno VALUES 
+  (15,'Gerald','Small','15-MAY-1962','Ball Street',122,'Samambaia', ' Dallas', null);
+INSERT INTO Aluno VALUES 
+  (16,'Arnold','Head','19-MAY-1967','Spring St',233,'Samambaia',' Dallas',null);
+INSERT INTO Aluno VALUES 
+  (17,'Helga','Pataki','11-MAR-1969','Holyoke St',101,'Samambaia', 'Dallas',null);
+INSERT INTO Aluno VALUES 
+  (18,'Naveen','Drew','23-MAY-1970','Elm St',198,'Morada Nova',' Philadelphia','naveen@ufg.br');
+INSERT INTO Aluno VALUES 
+  (19,'Carl','Reedy','21-JUN-1977','Ball St',213,'Morada Nova',' Philadelphia',null);
+INSERT INTO Aluno VALUES 
+  (20,'Sammy','Hall','11-JAN-1970','Main Street',433,'Florida',' Miami',null);
+INSERT INTO Aluno VALUES 
+  (21,'Red','Bacher','21-MAY-1980','Elm Street',196,'Florida', 'Miami', null);
+
+INSERT INTO Professor VALUES 
+  (20,'John','Smith','Fondren',731,'Campinas',' Houston',null, 30000);
+INSERT INTO Professor VALUES 
+  (21,'Alicia','Zelaya','Castle',3321, 'Centro',' Spring',null, 25000);
+INSERT INTO Professor VALUES 
+  (22,'Ramesh','Narayan','Fire Oak',971,'Fama',' Humble',null,38000);
+INSERT INTO Professor VALUES 
+  (23,'Joyce','Jordan','Rice Oak',5631,'Capuava',' Houston',null,25000);
+INSERT INTO Professor VALUES 
+  (24,'Ahmad','Jabbar','Dallas',980,'Tremendao', ' Houston',null,25000);
+
+INSERT INTO Departamento VALUES (1,'Musculação', 3);
+INSERT INTO Departamento VALUES (2,'Dança',2);
+INSERT INTO Departamento VALUES (3,'Lutas',1);
+
+INSERT INTO Modalidade VALUES (10,'Kick Boxing', 3);
+INSERT INTO Modalidade VALUES (20,'Karatê',3);
+INSERT INTO Modalidade VALUES (30,'Capoeira',3);
+
+INSERT INTO Modalidade VALUES (40,'Ballet', 2);
+INSERT INTO Modalidade VALUES (50,'Zumba',2);
+INSERT INTO Modalidade VALUES (60,'Forró',2);
+
+INSERT INTO Modalidade VALUES (70,'Aerobico', 1);
+INSERT INTO Modalidade VALUES (80,'Anaerobico',1);
+
+INSERT INTO Turma VALUES (11,'M',20, 10 );
+INSERT INTO Turma VALUES (12,'M',20, 20 );
+INSERT INTO Turma VALUES (13,'M',21, 30 );
+INSERT INTO Turma VALUES (14,'M',21, 40 );
+INSERT INTO Turma VALUES (15,'M',22, 50 );
+INSERT INTO Turma VALUES (16,'M',23, 60 );
+INSERT INTO Turma VALUES (17,'M',24, 70 );
+INSERT INTO Turma VALUES (18,'M',24, 80 );
+
+INSERT INTO Alunos_turma VALUES (1,11);
+INSERT INTO Alunos_turma VALUES (2,11);
+INSERT INTO Alunos_turma VALUES (3,11);
+
+INSERT INTO Alunos_turma VALUES (4,12);
+INSERT INTO Alunos_turma VALUES (5,12);
+INSERT INTO Alunos_turma VALUES (6,12);
+
+INSERT INTO Alunos_turma VALUES (7,13);
+INSERT INTO Alunos_turma VALUES (8,13);
+INSERT INTO Alunos_turma VALUES (9,13);
+
+INSERT INTO Alunos_turma VALUES (9,14);
+INSERT INTO Alunos_turma VALUES (10,14);
+INSERT INTO Alunos_turma VALUES (11,14);
+
+INSERT INTO Alunos_turma VALUES (12,15);
+INSERT INTO Alunos_turma VALUES (13,15);
+INSERT INTO Alunos_turma VALUES (14,15);
+
+INSERT INTO A_Telefone Values (1, 111111);
+INSERT INTO A_Telefone Values (2, 222222);
+INSERT INTO A_Telefone Values (3, 333333);
+INSERT INTO A_Telefone Values (4, 444444);
+INSERT INTO A_Telefone Values (5, 555555);
+INSERT INTO A_Telefone Values (6, 666666);
+INSERT INTO A_Telefone Values (7, 777777);
+INSERT INTO A_Telefone Values (8, 888888);
+INSERT INTO A_Telefone Values (9, 999999);
+INSERT INTO A_Telefone Values (10, 123455);
+INSERT INTO A_Telefone Values (11, 213454);
+INSERT INTO A_Telefone Values (12, 231453);
+INSERT INTO A_Telefone Values (13, 234150);
+INSERT INTO A_Telefone Values (14, 234510);
+INSERT INTO A_Telefone Values (15, 234561);
+INSERT INTO A_Telefone Values (16, 234567);
+INSERT INTO A_Telefone Values (17, 234568);
+INSERT INTO A_Telefone Values (18, 234511);
+INSERT INTO A_Telefone Values (19, 234569);
+INSERT INTO A_Telefone Values (20, 214568);
+INSERT INTO A_Telefone Values (21, 243590);
+
+
+INSERT INTO P_Telefone Values (20,329234);
+INSERT INTO P_Telefone Values (21,342190);
+INSERT INTO P_Telefone Values (22,314156);
+INSERT INTO P_Telefone Values (23,928101);
+INSERT INTO P_Telefone Values (24,029718);
+
+
+INSERT INTO Alunos_Turma Values (1,11);
+INSERT INTO Alunos_Turma Values (2,11);
+INSERT INTO Alunos_Turma Values (2,12);
+INSERT INTO Alunos_Turma Values (3,13);
+INSERT INTO Alunos_Turma Values (14,17);
+INSERT INTO Alunos_Turma Values (3,16);
+INSERT INTO Alunos_Turma Values (3,18);
+INSERT INTO Alunos_Turma Values (4,12);
+INSERT INTO Alunos_Turma Values (5,16);
+INSERT INTO Alunos_Turma Values (6,18);
+INSERT INTO Alunos_Turma Values (7,15);
+INSERT INTO Alunos_Turma Values (21,17);
+
+CREATE VIEW Dados_prof as select id_prof,nome,sobrenome,email from professor
+CREATE VIEW Dados_aluno as select id_aluno,nome,sobrenome,email from aluno natural join alunos_turma where id_turma = 11
+CREATE VIEW 
+ 
+
+
+//alunos por turma
+
+select count (id_aluno)
+from Alunos_turma
+Group By id_turma
+having id_turma = 12 
+
+//modalidade por departamento
+select count (mid)
+from modalidade 
+group by id_dep
+
+// telefone dos alunos
+select *
+from Dados_aluno natural join A_telefone 
+
+//modalidades do departamento 3
+select nome
+from (select* from modalidade where id_dep =3) as mod
+
+//turmas do professor id_prof= 20
+select nome, id_turma
+from(select * from turma where id_prof=20) as mod1 natural join professor
+
+
+// nome e e-mail de todos os membros vinculados na academia
+select nome,email
+from professor
+Union
+select nome, email
+from aluno
+
+// id e telefone de todos da academia
+select id_prof as id_geral , numero
+from P_telefone
+Union
+select *
+from A_telefone
+
+// todos os atributos de alunos com id_aluno {1,2,3,19}
+select *
+from Aluno
+where id_aluno in (1,2,3,19)
+
+// consulta existindo professor com id = 50
+select *
+from turma
+where exists (select id_prof from turma where id_prof =30)
+
+// salario maior ou igual q o primeiro salario da lista de professor
+select nome, salario
+from professor
+where  salario > some (select salario from professor)
+
+// modalidade karatê e ballet 
+select *
+from modalidade
+where nome in ('Karatê','Ballet')
+
+INSERT INTO Professor VALUES 
+  (25,'Edmilton','Junior','Rua dos bobos',000,'Jardim America',' Gyn','edm@acd.br', 40000); 
+  
+INSERT INTO Aluno Values
+  (22,'Dheiso','Araujo','2015-JUN-09','Rua 2',15,'Vila nova','Gyn','dheiso@inf.br');
+
+
+  delete from Aluno 
+  where id_Aluno = 18
+
+
+
+Select *
+From professor
---
+++ bd/tables-academia.sql
@@ -0,0 +1,94 @@
+﻿CREATE TABLE Aluno (
+    id_aluno integer not null,
+    nome character varying(15) not null,
+    sobrenome character varying(15) not null,
+    data_de_inicio date not null,
+    rua character varying(30),
+    numero integer not null,
+    bairro character varying(20),
+    cidade character varying(20),
+    email character varying(40),
+    
+    primary key (id_aluno)
+);
+
+CREATE TABLE A_Telefone (
+    id_aluno integer not null,
+    numero integer not null,
+    primary key (id_aluno)
+    
+);
+
+CREATE TABLE Departamento (
+    
+    id_dep integer not null,
+    nome character varying(15),
+    sala integer not null,
+    primary key (id_dep)
+);
+
+CREATE TABLE Modalidade (
+    mid integer not null,
+    nome character varying(15),
+    id_dep integer not null,
+
+    primary key (mid),
+    foreign key (id_dep) references Departamento(id_dep)
+
+);
+
+CREATE TABLE Professor (
+    id_prof integer not null,
+    nome character varying(15) not null,
+    sobrenome character varying(15) not null,
+    rua character varying(30),
+    numero integer not null,
+    bairro character varying(20),
+    cidade character varying(20),
+    email character varying(40),
+    salario numeric(10,2),
+    primary key (id_prof)
+);
+
+CREATE TABLE Turma (
+    id_turma integer not null,
+    turno character(1),
+    id_prof integer not null,
+    mid integer not null,
+    
+    primary key (id_turma),
+    foreign key (id_prof) references Professor(id_prof),
+    foreign key (mid) references Modalidade(mid)
+
+);
+
+CREATE TABLE Alunos_Turma (
+    id_aluno integer not null references Aluno(id_aluno),
+    nome character varying(15) not null,
+    sobrenome character varying(15) not null,
+    data_de_inicio date not null,
+    rua character varying(30),
+    numero integer not null,
+    bairro character varying(20),
+    cidade character varying(20),
+    email character varying(40),    
+    id_turma integer not null references Turma(id_turma),
+    turno character(1),
+    id_prof integer not null,
+    mid integer not null,
+
+    primary key (id_aluno,id_turma),
+    foreign key (id_prof) references Professor(id_prof),
+    foreign key (mid) references Modalidade(mid)
+
+);
+
+
+
+CREATE TABLE P_Telefone (
+    id_prof integer not null,
+    numero integer not null,
+    primary key (id_prof)
+);
+
+
